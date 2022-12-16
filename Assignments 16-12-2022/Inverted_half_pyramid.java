051486cc9343881f5ffb3824e63a6e2ca7a14e7b
Pritam7s <120620421+Pritam7s@users.noreply.github.com>
2022-12-16 15:54:37 +0530

    Create Inverted_half_pyramid.java

---
+++ Assignments 16-12-2022/Inverted_half_pyramid.java
@@ -0,0 +1,18 @@
+//WAP to print inverted pyramid
+package basic;
+
+public class Inverted_half_pyramid {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("* ");			
+			}
+			System.out.println();
+		}
+
+	}
+
+}
