ed17f57e2f0fd8493f86c58dc5a0d724eb3e1630
Pri._tom <120620421+Pritam7s@users.noreply.github.com>
2023-01-15 19:35:38 +0530

    Update BatchMain.java

--- Assignments 12-01-2023/BatchMain.java
+++ Assignments 12-01-2023/BatchMain.java
@@ -5,20 +5,21 @@ public class BatchMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("--- Welcome to Anudip Foundation ---");
-		System.out.println("");
+		System.out.println("\t--- Welcome to Anudip Foundation ---");
+		System.out.println("\t====================================\n");
 		int choice, ch;
 		Scanner sc = new Scanner(System.in);
 		BatchService batch = new BatchService();
 		
 		do {
-			System.out.println("Enter Your Choice: 1>Java  2>AWS  3>other  4>Exit");
+			System.out.println("Enter Your Choice: 1-->Java  2-->AWS  3-->Others  4-->Exit");
 			choice = sc.nextInt();
 			
 			switch(choice) {
 				case 1:
-					System.out.println("--- JAVA ---");
-					System.out.println("1>Enter Student Info \n2>Show Student Info \n3>Update Student Info");
+					System.out.println("\t--- JAVA ---");
+					System.out.println("\t~~~~~~~~~~~~");
+					System.out.println("1--> Enter Student Info \n2--> Show Student Info \n3--> Update Student Info");
 					ch = sc.nextInt();
 					switch(ch) {
 						case 1:
@@ -43,6 +44,29 @@ public class BatchMain {
 					break;
 				
 				case 2:
+					System.out.println("\t--- AWS ---");
+					System.out.println("\t~~~~~~~~~~~~");
+					System.out.println("1--> Enter Student Info \n2--> Show Student Details \n3--> Update Student Info");
+					ch = sc.nextInt();
+					switch(ch) {
+						case 1:
+							batch.awsStudentInfo();
+							System.out.println();
+							break;
+							
+						case 2:
+							batch.getAllAwsStudentDetails();
+							System.out.println();
+							break;
+							
+						case 3:
+							batch.updateAwsStudentInfo();
+							System.out.println();
+							break;
+							
+						default:
+							System.out.println("Wrong Input!!!");
+					}
 					
 					break;
 				
@@ -51,7 +75,8 @@ public class BatchMain {
 					break;
 					
 				case 4:
-					
+					System.out.println("Application will Terminate Now....");
+					System.exit(0);
 					break;
 					
 				default:
