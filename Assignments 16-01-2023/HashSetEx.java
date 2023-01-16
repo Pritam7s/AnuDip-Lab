0d64e0cb53eba7ba0f7558dd0ff5b8ff865e9896
Pri._tom <120620421+Pritam7s@users.noreply.github.com>
2023-01-16 15:34:04 +0530

    Create HashSetEx.java

---
+++ Assignments 16-01-2023/HashSetEx.java
@@ -0,0 +1,60 @@
+package collections;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class HashSetEx {
+
+	public static void main(String[] args) {
+		HashSet<String> hset = new HashSet<String>();
+		System.out.println(hset.hashCode());
+		
+		hset.add("dog");
+		hset.add("cat");
+		hset.add("tiger");
+		hset.add("monkey");
+		//hset.add("null");
+		System.out.println(hset);
+		System.out.println(hset.hashCode());
+		
+		hset.add("horse");
+		hset.add("dog");
+		System.out.println(hset);
+		Iterator itr = hset.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		
+		System.out.println();
+		System.out.println("Array: ");
+		String arr[] = new String[hset.size()];
+		hset.toArray(arr);
+		for(String s: arr) {
+			System.out.println(s);
+		}
+		System.out.println("Element in Index 2: "+arr[2]);
+		
+		System.out.println();
+		//creating an Array List
+		ArrayList<String> alist = new ArrayList<String>(hset);
+		System.out.println("ArrayList: "+alist);
+		
+		TreeSet<String> tset = new TreeSet<String>(hset);
+		tset.add("ant");
+		tset.add("Zebra");
+		//tset.add("99 Ant");
+		System.out.println("Treeset: "+tset);
+		
+		LinkedHashSet<Integer> lhset = new LinkedHashSet<Integer>();
+		lhset.add(44);
+		lhset.add(33);
+		lhset.add(55);
+		lhset.add(22);
+		lhset.add(1);
+		System.out.println("Linked Hash Set: "+lhset);
+	}
+
+}
