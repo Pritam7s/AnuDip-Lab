a594398b0130b6bdc776ad45e12789d78d3130e9
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-01-27 16:04:03 +0530

    Create MyThread.java

---
+++ Lab-Practice/27-01-2023/ThreadUsingRunnable/MyThread.java
@@ -0,0 +1,16 @@
+package multi_threading;
+
+public class MyThread implements Runnable{
+	
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " is Running...");
+	}
+
+	public static void main(String[] args) {
+		MyThread mt = new MyThread();
+		Thread t1 = new Thread(mt, "Java Thread");		//new state
+		t1.start();	
+	}
+
+}
