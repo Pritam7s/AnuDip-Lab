9bbfb3ff369413dee2ca7ec5f8830ddce19d4fa4
Pritom <120620421+Pritam7s@users.noreply.github.com>
2023-01-30 21:43:33 +0530

    Create StaticSynchronizationDemo.java

---
+++ Lab-Practice/30-01-2023/StaticSynchronization/StaticSynchronizationDemo.java
@@ -0,0 +1,74 @@
+package multi_threading;
+
+class PrintData2{
+	synchronized static void multi(int n){
+	synchronized(PrintData2.class){
+		for (int i=1;i<=5;i++){
+			System.out.println(n+" * "+i+"="+n*i);
+			try{
+				Thread.sleep(400);
+			}
+			catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	System.out.println("Rest of the code is not Synchronized");
+	}
+}
+
+class PrintData3{
+	synchronized static void add(int a,int b){
+		synchronized(PrintData2.class){
+			System.out.println("Addition: "+(a+b));
+			try {
+				Thread.sleep(400);
+			}
+			catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
+
+	
+public class StaticSynchronizationDemo {
+
+	public static void main(String[] args) {
+		
+		PrintData2 pd2= new PrintData2();
+		PrintData3 pd3= new PrintData3();
+		
+Thread t1 =new Thread() {
+		public void run(){
+				pd2.multi(19);
+			}
+		};
+		
+Thread t2 =new Thread() {
+		public void run(){
+				pd2.multi(100);
+			}
+};
+
+Thread t3 =new Thread() {
+	public void run(){
+		pd3.add(5,2);
+	}
+};
+
+
+Thread t4 =new Thread() {
+	public void run(){
+		pd3.add(500,200);
+	}
+};
+		
+
+	t1.start();
+	t2.start();
+	t3.start();
+	t4.start();
+	
+	}
+}
